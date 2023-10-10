14c45459046787c478b8cd8e9b1382d9165d6103
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-10-10 14:47:42 +0700

    PHP Dasar 1

---
+++ TipeDataString.php
@@ -0,0 +1,25 @@
+<?php
+
+echo 'Nama : ';
+echo 'Muhamad Iksan';
+echo "\n";
+
+
+echo "Nama : ";
+echo "Muhamad \t Iksan \t Keren \n";
+
+echo <<<EKO
+selamat belajar php
+sekarang, kita belajar tipe data string
+ini adalah cara ke-3 membuat string
+bisa menggunakan heredoc
+EKO;
+
+echo "\n";
+
+echo <<<'EKO'
+selamat belajar php
+sekarang, kita belajar tipe data string
+ini adalah cara ke-3 membuat string
+bisa menggunakan nowdoc
+EKO;
---
+++ Variable.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = 'Eko';
+$age = 22;
+
+echo "Nama : ";
+echo $name;
+echo "\n";
+
+echo "Umur : ";
+echo $age;
+echo "\n";
+
+$contoh = "eko";
+$$contoh = "kurniawan";
+
+echo 'Contoh : ';
+echo $contoh;
+echo "\n";
+
+echo 'eko : ';
+echo $eko;
+echo "\n";
